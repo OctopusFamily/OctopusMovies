22efad4431847274478c5e7019abcb9eb50e50bf
A10XY <86065368+A10XY@users.noreply.github.com>
2022-10-04 22:15:09 +0300

    edit Episode.kt

--- app/src/main/java/com/octopus/moviesapp/domain/model/Episode.kt
+++ app/src/main/java/com/octopus/moviesapp/domain/model/Episode.kt
@@ -3,10 +3,10 @@ package com.octopus.moviesapp.domain.model
 data class Episode(
     val name :String,
     val episodeNumber: Int,
+    val date: String,
     val voteAverage: Float,
-    val date:String,
     val voteCount: Int,
     val overview: String,
-    val crew : List<Person>,
     val trailerUrl: String,
+    val crew : List<Person>,
 )
