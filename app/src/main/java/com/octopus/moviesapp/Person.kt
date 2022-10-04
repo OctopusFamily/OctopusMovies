22075d9ea54f72be919a40593782df89757ce784
Dunya_4Mar <88485820+Dunya4mars@users.noreply.github.com>
2022-10-04 22:32:30 +0300

    make person data class

---
+++ app/src/main/java/com/octopus/moviesapp/Person.kt
@@ -0,0 +1,11 @@
+package com.octopus.moviesapp
+
+data class Person(
+    val personImageUrl: String,
+    val personName: String,
+    val biography: String,
+    val birthday: String,
+    val career: String,
+    val popularity: Float,
+    val placeOfBirth: String,
+    )
