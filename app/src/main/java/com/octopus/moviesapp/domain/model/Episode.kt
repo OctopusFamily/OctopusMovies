b4efd0b583c7134039da158855bd821de62a7ee6
Aizen-pc <tofymofy,55@gmail.com>
2022-10-04 22:11:31 +0300

    create episode data class

--- app/src/main/java/com/octopus/moviesapp/domain/model/Episode.kt
+++ app/src/main/java/com/octopus/moviesapp/domain/model/Episode.kt
@@ -2,9 +2,9 @@ package com.octopus.moviesapp.domain.model
 
 data class Episode(
     val name :String,
-    val date:String,
     val episodeNumber: Int,
     val voteAverage: Float,
+    val date:String,
     val voteCount: Int,
     val overview: String,
     val crew : List<Person>,
