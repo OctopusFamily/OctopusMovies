5b4aeba7c8971878f2072f239314b4bcd6bf514a
MoatazBadawy <moataz.egy101@gmail.com>
2022-10-05 18:45:06 +0300

    change Resource.kt packge postion

--- app/src/main/java/com/octopus/moviesapp/domain/sealed/Resource.kt
+++ app/src/main/java/com/octopus/moviesapp/domain/sealed/Resource.kt
@@ -1,8 +1,7 @@
-package com.octopus.moviesapp.util.statue
+package com.octopus.moviesapp.domain.sealed
 
 sealed class Resource<out T> {
     data class Success<T>(val data: T?) : Resource<T>()
     data class Error(val message: String) : Resource<Nothing>()
     object Loading : Resource<Nothing>()
-    object Empty : Resource<Nothing>()
 }
