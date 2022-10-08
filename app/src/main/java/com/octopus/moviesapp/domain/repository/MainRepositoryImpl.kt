aef41015f4ede5d18e2b45b6ed9e2c10899f337d
SHAMARDN <shamardn@gmail.com>
2022-10-08 20:18:08 +0200

    create fun of getPopularMovies() and refactore RequestState Sealed Class name to UiState

--- app/src/main/java/com/octopus/moviesapp/domain/repository/MainRepositoryImpl.kt
+++ app/src/main/java/com/octopus/moviesapp/domain/repository/MainRepositoryImpl.kt
@@ -1,4 +1,14 @@
 package com.octopus.moviesapp.domain.repository
 
+import com.octopus.moviesapp.data.remote.request.ApiClient
+import com.octopus.moviesapp.domain.mapper.MoviesMapper
+import com.octopus.moviesapp.domain.model.Movie
+
 class MainRepositoryImpl(): MainRepository {
+    val api = ApiClient.apiService
+    val moviesMapper = MoviesMapper()
+
+    override suspend fun getPopularMovies(): List<Movie> {
+        return moviesMapper.map(api.getPopularMovies(1).body()!!.items)
+    }
 }
--- app/src/main/java/com/octopus/moviesapp/util/BindingAdapters.kt
+++ app/src/main/java/com/octopus/moviesapp/util/BindingAdapters.kt
@@ -5,7 +5,7 @@ import android.widget.ImageView
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
-import com.octopus.moviesapp.domain.sealed.RequestState
+import com.octopus.moviesapp.domain.sealed.UiState
 
 @BindingAdapter(value = ["app:imageUrl"])
 fun loadImage(imageView: ImageView, imageUrl: String) {
@@ -13,16 +13,16 @@ fun loadImage(imageView: ImageView, imageUrl: String) {
 }
 
 @BindingAdapter(value = ["app:showIfRequestStateIsLoading"])
-fun <T> showIfRequestStateIsLoading(view: View, requestState: RequestState<T>) {
-    view.isVisible = requestState is RequestState.Loading
+fun <T> showIfRequestStateIsLoading(view: View, uiState: UiState<T>) {
+    view.isVisible = uiState is UiState.Loading
 }
 
 @BindingAdapter(value = ["app:showIfRequestStateIsSuccess"])
-fun <T> showIfRequestStateIsSuccess(view: View, requestState: RequestState<T>) {
-    view.isVisible = requestState is RequestState.Success
+fun <T> showIfRequestStateIsSuccess(view: View, uiState: UiState<T>) {
+    view.isVisible = uiState is UiState.Success
 }
 
 @BindingAdapter(value = ["app:showIfRequestStateIsError"])
-fun <T> showIfRequestStateIsError(view: View, requestState: RequestState<T>) {
-    view.isVisible = requestState is RequestState.Error
+fun <T> showIfRequestStateIsError(view: View, uiState: UiState<T>) {
+    view.isVisible = uiState is UiState.Error
 }
