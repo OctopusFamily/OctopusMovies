80ed4cea7364775415fcffa498678161fa9d8b52
A10XY <86065368+A10XY@users.noreply.github.com>
2022-10-09 01:58:29 +0300

    fix conflicts

--- app/src/main/java/com/octopus/moviesapp/ui/movies/MoviesViewModel.kt
+++ app/src/main/java/com/octopus/moviesapp/ui/movies/MoviesViewModel.kt
@@ -8,7 +8,8 @@ import javax.inject.Inject
 @HiltViewModel
 class MoviesViewModel @Inject constructor(
     private val repository: MainRepository
-) : ViewModel() {
+) : ViewModel(), MoviesClicksListener {
+    override fun onMovieClick(movieId: Int) {
 
-
+    }
 }
