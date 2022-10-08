8f75e74084ffaf132d7de59ec0198db069282931
Aizen-pc <tofymofy,55@gmail.com>
2022-10-09 01:32:37 +0300

    fix icon clicked

--- app/src/main/java/com/octopus/moviesapp/ui/movies/MoviesViewModel.kt
+++ app/src/main/java/com/octopus/moviesapp/ui/movies/MoviesViewModel.kt
@@ -1,6 +1,12 @@
 package com.octopus.moviesapp.ui.movies
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
-class MoviesViewModel : ViewModel() {
+class MoviesViewModel : ViewModel(), MoviesClicksListener {
+    override fun onMovieClick(movieId: Int) {}
+
+    fun onChipPopularCheck(checked: Boolean) {
+        Log.i("LOG_TAG", checked.toString())
+    }
 }
