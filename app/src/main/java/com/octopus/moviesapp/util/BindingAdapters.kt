2815fbede98a9c34aec0ea7f30677c1ca64eea6a
Mostafa Mohamed <tofymofy.55@gmail.com>
2022-10-09 22:30:46 +0300

    show data in tv_shows

--- app/src/main/java/com/octopus/moviesapp/util/BindingAdapters.kt
+++ app/src/main/java/com/octopus/moviesapp/util/BindingAdapters.kt
@@ -7,6 +7,7 @@ import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.octopus.moviesapp.domain.model.Genre
 import com.octopus.moviesapp.domain.sealed.UiState
 import java.text.SimpleDateFormat
 import java.util.*
@@ -33,12 +34,17 @@ fun <T> showIfRequestStateIsError(view: View, uiState: UiState<T>) {
 
 @SuppressLint("SimpleDateFormat")
 @BindingAdapter(value = ["app:releaseDate"])
-fun setReleaseDate (view: TextView, date: Date) {
+fun setReleaseDate(view: TextView, date: Date) {
     val formatDate = SimpleDateFormat("yyyy")
     view.text = formatDate.format(date).toString()
 }
 
 @BindingAdapter(value = ["app:voteAverage"])
-fun setVoteAverage (view: TextView, rating: Float) {
+fun setVoteAverage(view: TextView, rating: Float) {
     view.text = String.format("%.1f", rating).toDouble().toString()
+}
+
+@BindingAdapter(value = ["app:setGenresItem"])
+fun setGenresItem(view: TextView, items: List<Genre>) {
+    view.text = items.joinToString(" | ") { it.name }
 }
