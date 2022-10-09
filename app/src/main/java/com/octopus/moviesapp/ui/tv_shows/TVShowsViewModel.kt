d337d1acb9454db9baa9c65c33c087323a5b7c02
AmeerAmjed <pr.ameer98@gmail.com>
2022-10-09 20:24:40 +0300

    refactor tv show view model

--- app/src/main/java/com/octopus/moviesapp/ui/tv_shows/TVShowsViewModel.kt
+++ app/src/main/java/com/octopus/moviesapp/ui/tv_shows/TVShowsViewModel.kt
@@ -1,12 +1,12 @@
 package com.octopus.moviesapp.ui.tv_shows
 
-import androidx.lifecycle.ViewModel
 import com.octopus.moviesapp.domain.repository.MainRepository
+import com.octopus.moviesapp.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class TVShowsViewModel @Inject constructor(
     private val repository: MainRepository
-) : ViewModel() {
+) : BaseViewModel() {
 }
