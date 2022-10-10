c59fed9740caf8507220835aede0a12244ec3881
Asia sama <105972942+Asiasama710@users.noreply.github.com>
2022-10-10 11:25:07 -0700

    handle ui state in fragment genres

--- app/src/main/java/com/octopus/moviesapp/ui/genres/GenresViewModel.kt
+++ app/src/main/java/com/octopus/moviesapp/ui/genres/GenresViewModel.kt
@@ -1,11 +1,20 @@
 package com.octopus.moviesapp.ui.genres
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.octopus.moviesapp.domain.model.Genre
+import com.octopus.moviesapp.domain.model.Movie
 import com.octopus.moviesapp.domain.repository.MainRepository
+import com.octopus.moviesapp.domain.sealed.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 @HiltViewModel
 class GenresViewModel @Inject constructor(
     private val repository: MainRepository
 ) : ViewModel() {
+
+    private val _genreListState = MutableLiveData<UiState<List<Genre>>>(UiState.Loading)
+    val genreListState: LiveData<UiState<List<Genre>>> get() = _genreListState
+
 }
