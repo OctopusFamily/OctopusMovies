e226d7ffa6a5b5c30fa433c9ecefe7a999a12e81
AmeerAmjed <pr.ameer98@gmail.com>
2022-10-10 22:24:20 +0300

    refactor genre mapper

--- app/src/main/java/com/octopus/moviesapp/domain/mapper/GenresMapper.kt
+++ app/src/main/java/com/octopus/moviesapp/domain/mapper/GenresMapper.kt
@@ -3,12 +3,13 @@ package com.octopus.moviesapp.domain.mapper
 import com.octopus.moviesapp.data.remote.response.dto.GenreDTO
 import com.octopus.moviesapp.domain.model.Genre
 
-class GenresMapper: Mapper<List<GenreDTO>, List<Genre>> {
-    override fun map(input: List<GenreDTO>): List<Genre> {
+class GenresMapper : DuoMapper<List<GenreDTO>, List<Genre>, List<Genre>> {
+    override fun map(input: List<GenreDTO>, type: List<Genre>): List<Genre> {
         return input.map {
             Genre(
                 id = it.id ?: 0,
-                name = it.name ?: ""
+                name = it.name ?: "",
+                type = type,
             )
         }
     }
--- app/src/main/java/com/octopus/moviesapp/domain/model/Genre.kt
+++ app/src/main/java/com/octopus/moviesapp/domain/model/Genre.kt
@@ -3,4 +3,5 @@ package com.octopus.moviesapp.domain.model
 data class Genre(
     val id: Int,
     val name: String,
+    val type: List<Genre>,
 )
