5092bec7cdc60d9164b79821836fe2a19d40c5b9
SHAMARDN <shamardn@gmail.com>
2022-10-10 21:23:39 +0200

    create genresAdapter.kt and GenresClicksListener and impl. it in GenresViewModel

---
+++ app/src/main/java/com/octopus/moviesapp/ui/genres/GenresAdapter.kt
@@ -0,0 +1,12 @@
+package com.octopus.moviesapp.ui.genres
+
+import com.octopus.moviesapp.R
+import com.octopus.moviesapp.domain.model.Genre
+import com.octopus.moviesapp.ui.base.BaseAdapter
+
+class GenresAdapter (
+    genres: List<Genre>,
+    listener: GenresClicksListener,
+) : BaseAdapter<Genre>(genres, listener) {
+    override fun layoutId(): Int = R.layout.item_genre
+}
