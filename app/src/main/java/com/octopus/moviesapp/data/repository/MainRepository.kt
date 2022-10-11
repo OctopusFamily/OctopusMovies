64c2f904d658640935b014b51d5b4bd1fb6bd8d3
A10XY <86065368+A10XY@users.noreply.github.com>
2022-10-11 21:01:50 +0300

    refactor the project

--- app/src/main/java/com/octopus/moviesapp/data/repository/MainRepository.kt
+++ app/src/main/java/com/octopus/moviesapp/data/repository/MainRepository.kt
@@ -1,6 +1,6 @@
 package com.octopus.moviesapp.domain.repository
 
-import com.octopus.moviesapp.domain.enums.GenresList
+import com.octopus.moviesapp.domain.enums.GenresType
 import com.octopus.moviesapp.domain.model.Movie
 import com.octopus.moviesapp.domain.enums.MoviesCategory
 import com.octopus.moviesapp.domain.enums.TVShowsCategory
@@ -10,5 +10,5 @@ import com.octopus.moviesapp.domain.model.TVShow
 interface MainRepository {
     suspend fun getMoviesByCategory(moviesCategory: MoviesCategory, page: Int): List<Movie>
     suspend fun getTVShowsByCategory(tvShowCategory: TVShowsCategory, page: Int): List<TVShow>
-    suspend fun getGenresByList(genresByList: GenresList): List<Genre>
+    suspend fun getGenresByType(genresType: GenresType): List<Genre>
 }
--- app/src/main/java/com/octopus/moviesapp/domain/mapper/DuoMapper.kt
+++
@@ -1,5 +0,0 @@
-package com.octopus.moviesapp.domain.mapper
-
-interface DuoMapper<I1, I2, O> {
-    fun map(firstInput: I1, secondInput: I2): O
-}
