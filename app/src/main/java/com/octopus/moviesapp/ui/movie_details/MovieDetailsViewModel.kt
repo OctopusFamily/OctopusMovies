06827e85da9dee500e8fc0609f7446b217aa019d
AmeerAmjed <pr.ameer98@gmail.com>
2022-10-11 17:37:31 +0300

    fixed

--- app/src/main/java/com/octopus/moviesapp/ui/movie_details/MovieDetailsViewModel.kt
+++ app/src/main/java/com/octopus/moviesapp/ui/movie_details/MovieDetailsViewModel.kt
@@ -2,7 +2,8 @@ package com.octopus.moviesapp.ui.movie_details
 
 import com.octopus.moviesapp.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class MovieDetailsViewModel : BaseViewModel() {
+class MovieDetailsViewModel @Inject constructor() : BaseViewModel() {
 }
