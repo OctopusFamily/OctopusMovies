a717b3f5143beec8e022ee213cbef2ab24cc844c
A10XY <86065368+A10XY@users.noreply.github.com>
2022-10-20 00:27:41 +0300

    fix some issue in SettingsViewModel.kt

--- app/src/main/java/com/octopus/moviesapp/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/octopus/moviesapp/ui/settings/SettingsViewModel.kt
@@ -5,18 +5,23 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.octopus.moviesapp.data.local.DataStorePref
 import com.octopus.moviesapp.domain.types.Language
 import com.octopus.moviesapp.domain.types.Theme
 import com.octopus.moviesapp.util.Constants
 import com.octopus.moviesapp.util.Event
 import com.octopus.moviesapp.util.SettingsService
+import com.octopus.moviesapp.util.extensions.postEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SettingsViewModel @Inject constructor(@ApplicationContext context: Context,private val dataStorePreferences: DataStorePref): ViewModel() {
+class SettingsViewModel @Inject constructor(
+    @ApplicationContext context: Context,
+    private val dataStorePreferences: DataStorePref
+    ): ViewModel() {
 
     private val _languageChoiceClicked = MutableLiveData(Event(false))
     val languageChoiceClicked: LiveData<Event<Boolean>> get() = _languageChoiceClicked
