4d5022dcb732b00b10aa4d4bce56de0c0ef5759d
A10XY <86065368+A10XY@users.noreply.github.com>
2022-10-19 21:28:11 +0300

    improve code quality and fix theme issue

--- app/src/main/java/com/octopus/moviesapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/octopus/moviesapp/ui/main/MainActivity.kt
@@ -7,15 +7,28 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.octopus.moviesapp.R
+import com.octopus.moviesapp.data.local.DataStorePref
 import com.octopus.moviesapp.databinding.ActivityMainBinding
-import com.octopus.moviesapp.ui.settings.SettingsViewModel
+import com.octopus.moviesapp.domain.types.Theme
+import com.octopus.moviesapp.util.Constants
 import com.octopus.moviesapp.util.SettingsService
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
+
+    @Inject lateinit var dataStorePreferences: DataStorePref
+    private val settingsService = SettingsService
+
+    init {
+        setTheme()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,5 +49,15 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    private fun setTheme() {
+        CoroutineScope(Dispatchers.Main).launch {
+            dataStorePreferences.readString(Constants.DARK_MODE).collect { currentTheme ->
+                if (currentTheme != null) {
+                    settingsService.updateAppTheme(Theme.valueOf(currentTheme))
+                }
+            }
+        }
     }
 }
