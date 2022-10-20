e2a7a997deeb5ae679dbe61523411e4154aaa8de
Aizen-pc <tofymofy,55@gmail.com>
2022-10-20 23:14:07 +0300

    fix issue double items when rotate

--- app/src/main/java/com/octopus/moviesapp/ui/movie_details/MovieDetailsViewModel.kt
+++ app/src/main/java/com/octopus/moviesapp/ui/movie_details/MovieDetailsViewModel.kt
@@ -2,6 +2,7 @@ package com.octopus.moviesapp.ui.movie_details
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import com.octopus.moviesapp.data.repository.MoviesRepository
 import com.octopus.moviesapp.domain.model.Cast
@@ -24,6 +25,7 @@ import javax.inject.Inject
 class MovieDetailsViewModel @Inject constructor(
     private val moviesRepository: MoviesRepository,
     private val connectionTracker: ConnectionTracker,
+    saveStateHandle: SavedStateHandle,
 ) : BaseViewModel(), NestedGenresListener, NestedCastListener {
 
     private val _movieDetailsState = MutableLiveData<UiState<MovieDetails>>(UiState.Loading)
@@ -58,8 +60,14 @@ class MovieDetailsViewModel @Inject constructor(
     private val _navigateToPersonDetails = MutableLiveData<Event<Int>>()
     val navigateToPersonDetails: LiveData<Event<Int>> get() = _navigateToPersonDetails
 
+    private val args = MovieDetailsFragmentArgs.fromSavedStateHandle(saveStateHandle)
+
+init {
+    loadMovieDetails(args.movieId)
+}
+
     private var movieID = 0
-    fun loadMovieDetails(movieId: Int) {
+    private fun loadMovieDetails(movieId: Int) {
         movieID = movieId
 
         viewModelScope.launch {
@@ -73,13 +81,13 @@ class MovieDetailsViewModel @Inject constructor(
     }
 
     private fun getMovieDetails() {
-        getMovieDetails(movieID)
-        getMovieCast(movieID)
-        getMovieTrailer(movieID)
+        getMovieDetails(args.movieId)
+        getMovieCast(args.movieId)
+        getMovieTrailer(args.movieId)
     }
 
     fun tryLoadMovieDetailsAgain() {
-        loadMovieDetails(movieID)
+        loadMovieDetails(args.movieId)
     }
 
     fun onLoadMovieDetailsSuccess(movieDetails: MovieDetails) {
