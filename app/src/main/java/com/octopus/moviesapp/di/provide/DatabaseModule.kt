14114184e1a38c728bc5f70cef8d19428ce8efeb
MoatazBadawy <moataz.egy101@gmail.com>
2022-10-23 20:54:16 +0200

    update inject DatabaseModule.kt

--- app/src/main/java/com/octopus/moviesapp/di/provide/DatabaseModule.kt
+++ app/src/main/java/com/octopus/moviesapp/di/provide/DatabaseModule.kt
@@ -1,6 +1,7 @@
 package com.octopus.moviesapp.di.provide
 
 import android.content.Context
+import com.octopus.moviesapp.data.local.database.dao.MoviesDao
 import com.octopus.moviesapp.data.local.database.db.MoviesDatabase
 import dagger.Module
 import dagger.Provides
@@ -15,10 +16,13 @@ object DatabaseModule {
 
     @Singleton
     @Provides
-    fun provideDatabase(@ApplicationContext appContext: Context) =
-        MoviesDatabase.getInstance(appContext)
+    fun provideDatabase(@ApplicationContext appContext: Context): MoviesDatabase {
+        return MoviesDatabase.getInstance(appContext)
+    }
 
     @Singleton
     @Provides
-    fun provideMoviesDao(database: MoviesDatabase) = database.moviesDao()
+    fun provideMoviesDao(database: MoviesDatabase): MoviesDao {
+        return database.moviesDao()
+    }
 }
