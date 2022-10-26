322d084654a805424d810bde5532601a4543109a
abdbashar <abdullahbashar98@gmail.com>
2022-10-26 19:51:03 +0300

    set scroll positions 0 for recycler view when items added

--- app/src/main/java/com/octopus/moviesapp/util/BindingAdapters.kt
+++ app/src/main/java/com/octopus/moviesapp/util/BindingAdapters.kt
@@ -3,12 +3,10 @@ package com.octopus.moviesapp.util
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
 import android.view.View
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.RadioGroup
-import android.widget.TextView
+import android.widget.*
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.airbnb.lottie.LottieAnimationView
 import com.bumptech.glide.Glide
 import com.google.android.material.chip.ChipGroup
@@ -17,6 +15,7 @@ import com.octopus.moviesapp.ui.search.ChipGroupClickListener
 import kotlinx.android.synthetic.main.fragment_search.view.*
 import com.octopus.moviesapp.domain.types.Language
 import com.octopus.moviesapp.domain.types.Theme
+import com.octopus.moviesapp.ui.base.BaseAdapter
 import com.octopus.moviesapp.util.extensions.getSelectedChipIndex
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -216,4 +215,10 @@ fun <T> hideWhenEmpty(view: View, items: List<T>?) {
 @BindingAdapter(value = ["app:showIfEmpty"])
 fun <T> showWhenEmpty(view: View, items: List<T>?) {
     view.isVisible = items?.isEmpty() ?: false
+}
+
+@BindingAdapter(value = ["app:items"])
+fun <T> setRecyclerItems(view: RecyclerView, items: List<T>?) {
+    (view.adapter as BaseAdapter<T>?)?.setItems(items ?: emptyList())
+    view.scrollToPosition(0)
 }
