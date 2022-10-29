e97ed22c5cd86f201df35c9889575d1e3f69c4f9
Asia sama <sehunexo710@gmail.com>
2022-10-29 19:17:46 +0300

    add tvshow genre use case

---
+++ app/src/main/java/com/octopus/moviesapp/data/remote/pagingsource/TVShowsGenrePagingSource.kt
@@ -0,0 +1,16 @@
+package com.octopus.moviesapp.data.remote.pagingsource
+
+import com.octopus.moviesapp.data.remote.service.TMDBApiService
+import com.octopus.moviesapp.domain.mapper.TVShowsMapper
+import com.octopus.moviesapp.domain.model.TVShow
+import com.octopus.moviesapp.domain.types.TVShowsCategory
+
+class TVShowsGenrePagingSource(
+    private val tmdbApiService: TMDBApiService,
+    private val genreId: Int,
+    private val tvShowsMapper: TVShowsMapper,
+) : BasePagingSource<TVShow>() {
+    override suspend fun getData(page: Int): List<TVShow> {
+        return tvShowsMapper.mapList(tmdbApiService.getTVShowsByGenresId(genreId, page).items)
+    }
+}
--- app/src/main/java/com/octopus/moviesapp/ui/tv_shows_genre/TVShowsGenreViewModel.kt
+++ app/src/main/java/com/octopus/moviesapp/ui/tv_shows_genre/TVShowsGenreViewModel.kt
@@ -3,28 +3,35 @@ package com.octopus.moviesapp.ui.tv_shows_genre
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.cachedIn
+import androidx.paging.map
 import com.octopus.moviesapp.data.repository.genres.GenresRepository
 import com.octopus.moviesapp.domain.model.TVShow
+import com.octopus.moviesapp.domain.use_case.GetTVShowsByGenreIdPagingSourceUseCase
 import com.octopus.moviesapp.ui.base.BaseViewModel
 import com.octopus.moviesapp.ui.tv_shows.TVShowsClicksListener
+import com.octopus.moviesapp.ui.tv_shows.uistate.TVShowUiState
+import com.octopus.moviesapp.ui.tv_shows.uistate.TVShowsMainUiState
+import com.octopus.moviesapp.ui.tv_shows_genre.uistate.TVShowsGenreMainUiState
 import com.octopus.moviesapp.util.ConnectionTracker
 import com.octopus.moviesapp.util.Constants
 import com.octopus.moviesapp.util.Event
 import com.octopus.moviesapp.util.UiState
 import com.octopus.moviesapp.util.extensions.postEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class TVShowsGenreViewModel @Inject constructor(
-    private val genresRepository: GenresRepository,
-    private val connectionTracker: ConnectionTracker,
+    private val getTVShowsByGenreIdPagingSourceUseCase: GetTVShowsByGenreIdPagingSourceUseCase,
 ) : BaseViewModel(), TVShowsClicksListener {
 
-    private val _tvShowGenreState = MutableLiveData<UiState<List<TVShow>>>(UiState.Loading)
-    val tvShowGenreState: LiveData<UiState<List<TVShow>>> get() = _tvShowGenreState
+    private val _tvShowGenreState = MutableStateFlow(TVShowsGenreMainUiState())
+    val tvShowGenreState: StateFlow<TVShowsGenreMainUiState> get() = _tvShowGenreState
 
     private val _genreName = MutableLiveData("")
     val genreName: LiveData<String> get() = _genreName
@@ -39,31 +46,19 @@ class TVShowsGenreViewModel @Inject constructor(
     fun loadTVShow(genreId: Int, genreName: String) {
         genreID = genreId
         _genreName.postValue(genreName)
-        getLoadTVShow()
+        fetchTVShowByGenreId(genreID)
+
     }
 
-    private fun getLoadTVShow() {
-        viewModelScope.launch {
-            if (connectionTracker.isInternetConnectionAvailable()) {
-                getTVShowByGenreId(genreID)
-            } else {
-                _tvShowGenreState.postValue(UiState.Error(Constants.ERROR_INTERNET))
-            }
+
+    private fun fetchTVShowByGenreId(genreId: Int) {
+        val tvShowsUiStateFlow = Pager(PagingConfig(Constants.ITEMS_PER_PAGE, enablePlaceholders = true))
+        { getTVShowsByGenreIdPagingSourceUseCase(genreId) }.flow.cachedIn(viewModelScope).map { pagingData ->
+            pagingData.map { tvShow -> tvShow.asTVShowUiState() }
         }
-
+        _tvShowGenreState.update { it.copy(tvShowsUiState = tvShowsUiStateFlow) }
     }
 
-    fun tryLoadTVShow() {
-        getLoadTVShow()
-    }
-
-    private fun getTVShowByGenreId(genreId: Int) {
-        viewModelScope.launch {
-            wrapResponse { genresRepository.getListOfTVShowsByGenresId(genreId) }.collectLatest {
-                _tvShowGenreState.postValue(it)
-            }
-        }
-    }
 
     fun onNavigateBackClick() {
         _navigateBack.postEvent(true)
@@ -71,6 +66,16 @@ class TVShowsGenreViewModel @Inject constructor(
 
     override fun onTVShowClick(tvShowId: Int) {
         _navigateToTVShowDetails.postEvent(tvShowId)
+    }
+
+    private fun TVShow.asTVShowUiState(): TVShowUiState {
+        return TVShowUiState(
+            id = id,
+            title = title,
+            imageUrl = posterImageUrl,
+            released = started,
+            rating = voteAverage,
+        )
     }
 
 }
