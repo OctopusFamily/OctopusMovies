188c74a6a1cfb3cf01ef6bd9f12fa14fe2bec82e
abdbashar <abdullahbashar98@gmail.com>
2022-11-02 18:24:42 +0300

    add get list details use case in list details view model

--- app/src/main/java/com/octopus/moviesapp/ui/lists/ListDetailsInteractionListener.kt
+++ app/src/main/java/com/octopus/moviesapp/ui/lists/ListDetailsInteractionListener.kt
@@ -1,9 +1,8 @@
 package com.octopus.moviesapp.ui.lists
 
-import com.octopus.moviesapp.domain.model.ListDetails
 import com.octopus.moviesapp.ui.base.BaseInteractionListener
+import com.octopus.moviesapp.ui.lists.listsUIState.ListDetailsUIState
 
 interface ListDetailsInteractionListener : BaseInteractionListener {
-    fun onItemClick(item: ListDetails)
-
+    fun onItemClick(item: ListDetailsUIState)
 }
