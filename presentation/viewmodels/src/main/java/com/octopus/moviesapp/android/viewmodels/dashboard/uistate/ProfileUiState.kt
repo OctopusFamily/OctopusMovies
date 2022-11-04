db52e57aae88ed779ca672c7fa1fa8f126bc22eb
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2022-11-05 00:28:02 +0300

    move dashboard ui state

---
+++ presentation/viewmodels/src/main/java/com/octopus/moviesapp/android/viewmodels/dashboard/uistate/ProfileUiState.kt
@@ -0,0 +1,7 @@
+package com.octopus.moviesapp.android.viewmodels.dashboard.uistate
+
+data class ProfileUiState(
+    val name: String,
+    val username: String,
+    val profileImageUrl: String,
+)
